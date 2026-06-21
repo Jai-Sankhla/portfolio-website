823b9f455d92323b27e37f42309070463799f934
Jai Sankhla <jaisankhla@gmail.com>
2026-06-21 13:43:54 +0530

    fix: reduce scroll cue bottom gap to 2

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -153,7 +153,7 @@ export default function HeroSection() {
         {/* Scroll cue */}
         <motion.div
           style={{ opacity: cueOpacity }}
-          className="absolute bottom-6 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
+          className="absolute bottom-2 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
         >
           <span className="text-xs text-[#707072]">Scroll to explore</span>
           <motion.div
